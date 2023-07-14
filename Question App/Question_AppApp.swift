ec5bfc9423bf84cd213907678484a8fd9378704f
Kelsey McIntyre <kelsrmcintyre@gmail.com>
2023-07-14 15:56:45 -0400

    Initial Commit

---
+++ Question App/Question_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_AppApp.swift
+//  Question App
+//
+//  Created by Kelsey McIntyre on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Question_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
